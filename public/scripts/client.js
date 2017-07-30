abe0971fe47a51270da57c80eabd546a04e764c7
Chris Stanton <cstanton0760@yahoo.com>
2017-07-29 19:42:03 -0500

    added waypoint javascript code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,7 @@
 
 console.log("working");
 var waypoint = new Waypoint({
-  element: document.getElementById('waypoint-two'),
+  element: document.getElementById('basic-waypoint'),
   handler: function(direction) {
     console.log('Scrolled to waypoint-one')
   }
