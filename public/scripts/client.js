b77432477ae7ae07bf824b3af76112ae73c5fa41
Chris Stanton <cstanton0760@yahoo.com>
2017-07-30 01:59:14 -0500

    added header effect

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,37 @@
 
 console.log("client.js sourced!");
 
-// var waypoint = new Waypoint({
-//   element: document.getElementById('waypoint-two'),
-//   handler: function() {
-//     alertify.success("<div> Waypoint Two </div>");
-//   }
-// });
+var waypoint = new Waypoint({
+  element: document.getElementById('waypoint-one'),
+  handler: function(direction) {
+    alertify.log("Minneapolis, MN");
+  },
+  // offset: 400
+})
 
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint-two'),
   handler: function(direction) {
-    alertify.log("Waypoint Two");
+    alertify.log("St.paul, MN");
+  },
+  offset: 200
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('waypoint-three'),
+  handler: function(direction) {
+    alertify.log("Minneapolis, MN");
+  },
+  offset: 200
+})
+
+var waypoint = new Waypoint({
+  element: document.getElementById('section-four'),
+  handler: function(direction) {
+    // alertify is need
+    alertify
+    .closeLogOnClick(true)
+    .log("<a href='#section-one'><div>  TOP <img src='../public/assets/images/arrow-up.ico'></div></a>")
   },
   offset: 400
 })
