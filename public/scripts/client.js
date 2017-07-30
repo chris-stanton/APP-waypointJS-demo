13e518b988be89eea45335fa4eba0019a383844d
Chris Stanton <cstanton0760@yahoo.com>
2017-07-29 22:32:13 -0500

    added alertify

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@ console.log("client.js sourced!");
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint-two'),
   handler: function() {
-    alertify.success("Success log message");
+    alertify.success("<div class='alertImages'>" + "<div>Success log message</div>" +
+                      "<img src='../public/assets/images/fire.ico'>" + "</div>");
   }
 });
