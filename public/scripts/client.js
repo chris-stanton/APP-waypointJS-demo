b0b05191a0c97b20970dfe0e00c65246766aa103
Chris Stanton <cstanton0760@yahoo.com>
2017-07-30 12:28:05 -0500

    added input and button click listhener

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,26 +4,43 @@ console.log("client.js sourced!");
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint-one'),
   handler: function(direction) {
-    alertify.log("Minneapolis, MN");
+    alertify.log("Log Alert");
   },
   // offset: 400
-})
+});
 
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint-two'),
   handler: function(direction) {
-    alertify.log("St.paul, MN");
+    alertify.success("Success Alert");
   },
   offset: 200
-})
+});
 
 var waypoint = new Waypoint({
   element: document.getElementById('waypoint-three'),
   handler: function(direction) {
-    alertify.log("Minneapolis, MN");
+    alertify.error("Error Alert");
   },
   offset: 200
-})
+});
+
+var waypoint = new Waypoint({
+  element: document.getElementById('section-four'),
+  handler: function(direction) {
+    alertify.log("<div>" +
+                    "<input id='input' type='text'>" +
+                    "<button type='button' onclick='inputButton()'></button>" +
+                 "</div>");
+  },
+  offset: 250
+});
+
+// button click listener
+function inputButton (){
+  var input = document.getElementById('input').value;
+  console.log(input);
+};
 
 var waypoint = new Waypoint({
   element: document.getElementById('section-four'),
@@ -31,10 +48,12 @@ var waypoint = new Waypoint({
     // alertify is need
     alertify
     .closeLogOnClick(true)
-    .log("<a href='#section-one'><div>  TOP <img src='../public/assets/images/arrow-up.ico'></div></a>")
+    .log("<a href='#section-one'><div> Click To Go <img src='../public/assets/images/arrow-up.ico'></div></a>")
   },
   offset: 400
-})
+});
+
+
 
 
 
